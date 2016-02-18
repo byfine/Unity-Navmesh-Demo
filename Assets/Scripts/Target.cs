4134216b997019b4287a2d7307e3e9f8f573da00
byfine <7byfine@gmail.com>
2016-02-18 23:30:21 +0800

    first commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    private NavMeshAgent[] navAgents;
+
+    void Start()
+    {
+        navAgents = FindObjectsOfType(typeof(NavMeshAgent)) as NavMeshAgent[];
+    }
+
+    void UpdateTargets(Vector3 targetPosition)
+    {
+        foreach (NavMeshAgent agent in navAgents)
+        {
+            agent.destination = targetPosition;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Vector3 targetPosition = hitInfo.point;
+                UpdateTargets(targetPosition);
+                transform.position = targetPosition + new Vector3(0, 3, 0);
+            }
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Unity-Navmesh-Demo
+Unity Navmesh Demo
+
+测试 Unity 5 的 Navmesh 功能，实现layer、obstacle、off mesh link等功能。
